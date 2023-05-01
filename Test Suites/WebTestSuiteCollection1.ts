29da521770718a2525cae485afe7c06cc2e5c690
pravi <pravi@Pravins>
2023-05-01 12:34:24 +0530

    Collection to run in Chrome

--- Test Suites/WebTestSuiteCollection1.ts
+++ Test Suites/WebTestSuiteCollection1.ts
@@ -12,7 +12,7 @@
             <groupName>Web Desktop</groupName>
             <profileName>default</profileName>
             <requireConfigurationData>false</requireConfigurationData>
-            <runConfigurationId>Edge Chromium</runConfigurationId>
+            <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/WebTestSuite1</testSuiteEntity>
