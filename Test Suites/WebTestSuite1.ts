0eedecbe8c547215e9c7a66b05691c91e75a2e01
pravi <pravi@Pravins>
2023-05-01 11:56:07 +0530

    With Test Suite and Collection

---
+++ Test Suites/WebTestSuite1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>WebTestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>pravinrajpaul@gmail.com;pravin.raveendran@katalon.com;</mailRecipient>
+   <numberOfRerun>1</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>false</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ec3f61a3-67f0-4b3a-8d5c-3ce11dcaa5bd</testSuiteGuid>
+   <testCaseLink>
+      <guid>4a27df78-254c-4b94-be3f-31bde6e91236</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WebTestCase1/Step1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
