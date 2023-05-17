6fda36624dbb92604d3c49097d5f91d05ad5029b
pravinrajpaul <pravinrajpaul@192.168.1.7>
2023-05-17 18:32:55 +0530

    Add New Android Mobile Test Suite

---
+++ Test Suites/AndroidNativeTestsuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>AndroidNativeTestsuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>478a4d9f-a06c-4c3c-9587-7d2334d71a8a</testSuiteGuid>
+   <testCaseLink>
+      <guid>f7e398b2-10d0-4a45-94cd-532223d11abb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WebTestCases/MobileTestCaseAndroid</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
