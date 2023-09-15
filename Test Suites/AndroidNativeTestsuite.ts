b209d31a8a7b2fbc9a7137dba42b3b0139794509
geetyavin <geetyavin@gmail.com>
2023-09-15 13:53:54 +0530

    Remote capabilities removed

--- Test Suites/AndroidNativeTestsuite.ts
+++ Test Suites/AndroidNativeTestsuite.ts
@@ -12,10 +12,10 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>478a4d9f-a06c-4c3c-9587-7d2334d71a8a</testSuiteGuid>
    <testCaseLink>
-      <guid>f7e398b2-10d0-4a45-94cd-532223d11abb</guid>
+      <guid>ab015d2c-f30b-4676-9f9f-4ff7f61228c0</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MobileTests/MobileTestCaseAndroid</testCaseId>
+      <testCaseId>Test Cases/MobileTests/MobileTestCaseAndroidCloud</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
