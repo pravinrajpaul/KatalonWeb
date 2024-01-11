118a5cb90ebb0847af20df2a723f7e975101814b
geetyavin <geetyavin@gmail.com>
2024-01-11 19:01:05 +0530

    Testsuite collection for Test Cloud

---
+++ Object Repository/TriggerTestopsSchedule.rs
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>TriggerTestopsSchedule</name>
+   <tag></tag>
+   <elementGuidId>ac4eba2d-c856-4181-a57a-c2c79545ffe9</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <autoUpdateContent>true</autoUpdateContent>
+   <connectionTimeout>0</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization</name>
+      <type>Main</type>
+      <value>Basic cHJhdmluLnJhdmVlbmRyYW5Aa2F0YWxvbi5jb206ODFhYjVlNmEtYzYyOS00NTdjLThlYjctY2RmZWFjNDY1NmYx</value>
+      <webElementGuid>c5cdeb0c-607e-428b-89e5-71f818225582</webElementGuid>
+   </httpHeaderProperties>
+   <katalonVersion>9.1.0</katalonVersion>
+   <maxResponseSize>0</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>PUT</restRequestMethod>
+   <restUrl>https://testops.katalon.io/api/v1/run-configurations/606490/execute</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>0</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
--- Test Suites/AndroidNativeTestSuiteCollection.ts
+++ Test Suites/AndroidNativeTestSuiteCollection.ts
@@ -12,24 +12,6 @@
             <groupName>Web Desktop</groupName>
             <profileName>default</profileName>
             <requireConfigurationData>true</requireConfigurationData>
-            <runConfigurationData>
-               <entry>
-                  <key>testCloudMobile</key>
-                  <value>{
-  &quot;id&quot;: &quot;samsung_galaxy_s21_ultra_5g&quot;,
-  &quot;deviceId&quot;: &quot;Galaxy_S21_Ultra_5G_android_13&quot;,
-  &quot;isTablet&quot;: false,
-  &quot;manufacturer&quot;: [
-    &quot;Samsung&quot;
-  ],
-  &quot;name&quot;: &quot;Galaxy S21 Ultra 5G&quot;,
-  &quot;osVersion&quot;: &quot;13&quot;,
-  &quot;os&quot;: &quot;Android&quot;,
-  &quot;timeout&quot;: 15,
-  &quot;usingTunnel&quot;: false
-}</value>
-               </entry>
-            </runConfigurationData>
             <runConfigurationId>TestCloud</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
